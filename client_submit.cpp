8b696f256e785528854b5f1c33b0ab73b50eb97c
msy2008 <963858700@qq.com>
2024-10-15 14:37:23 +0800

    For auxpow: solve the problem of inconsistency between the block height stored in the database and the actual block height

--- client_submit.cpp
+++ client_submit.cpp
@@ -213,7 +213,11 @@ static void client_do_submit(YAAMP_CLIENT *client, YAAMP_JOB *job, YAAMP_JOB_VAL
 			if(b)
 			{
 				debuglog("*** ACCEPTED %s %d (+1)\n", coind_aux->name, coind_aux->height);
-
+				
+				// Increment the height of the auxiliary chain 
+                                coind_aux->height += 1;
+				
+				// Add the block to the database  
 				block_add(client->userid, client->workerid, coind_aux->id, coind_aux->height, target_to_diff(coin_target_aux),
 					target_to_diff(hash_int), current_aux->hash, "", 0);
 			}
